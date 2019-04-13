6712a0afe1fb2ba2faeedf6d4408e681744d94a6
Jaimelll <jaduran0102@gmail.com>
2019-04-13 10:28:26 -0500

    hoja23 6

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,18 +6,7 @@ class Ability
   def initialize(user)
   # Define abilities for the passed in user here. For example:
     #
-    user ||= AdminUser.new 
     
-      case user.categoria 
-          when 1,0
-              can :manage, :all
-          when 2 
-           
-            can :read, ActiveAdmin::Page, :name =>"Dashboard"
-            can [:create,:read,:update,:destroy], [Product]
-          else 
-            can :read, ActiveAdmin::Page, :name =>"Dashboard"
-       end        
-              
+        can :manage, :all
   end
 end
--- db/migrate/20190411175524_add_categoria_to_admin_users.rb
+++
@@ -1,6 +0,0 @@
-class AddCategoriaToAdminUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :admin_users, :categoria, :integer,  default: 0,  null: false
-    add_column :admin_users, :periodo, :integer,  default: 0,  null: false
-  end
-end

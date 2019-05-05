a2fcc48fcae80ec149a62e287f1fb40478cc7344
Jaimelll <jaduran0102@gmail.com>
2019-05-05 15:01:00 -0500

    pyg6

--- app/admin/epyg.rb
+++ app/admin/epyg.rb
@@ -70,7 +70,7 @@ ActiveAdmin.register_page "Epyg" do
                      
                       
                       
-                      vacumst=number_with_delimiter(@vacums.to_i, delimiter: ",") 
+                      vacumst=number_to_currency(@vacums, unit: "S/ ", separator: ".", delimiter: ",", precision: 2)
                       vspane=vn+' Total importe ='+vacumst
                       panel  vspane, :class => 'text-right' do  
                       end
